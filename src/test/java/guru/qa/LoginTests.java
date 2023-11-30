4e35cad5e8a547523b28d0b7c2fbe5c9b3586f68
Egor Chubkov <ea.chubkov@gmail.com>
2023-11-30 23:24:29 +0300

    hw 10

---
+++ src/test/java/guru/qa/LoginTests.java
@@ -0,0 +1,61 @@
+package guru.qa;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LoginTests extends TestBase {
+    @ValueSource(strings = {
+            "#userName",
+            "#password"
+    })
+    @ParameterizedTest()
+    @Tag("login")
+    void enableInputField(String id) {
+        open("/login");
+        $(id).isEnabled();
+    }
+
+    @CsvSource(value = {
+            "4dm1n, 12345678",
+            "email@gmail.com, fdiYVDye323ujH"
+    })
+    @DisplayName("Ошибка 'Неправильное имя пользователя или пароль!' при вводе неправильного логина или пароля")
+    @ParameterizedTest()
+    @Tag("login")
+    void invalidUsernameOrPasswordTest(String login, String password) {
+        open("/login");
+        $("#userName").setValue(login);
+        $("#password").setValue(password);
+        $("#login").click();
+        $("#output").shouldHave(text("Invalid username or password!"));
+    }
+
+    static Stream<Arguments> navigationPanelTest() {
+        return Stream.of(
+                Arguments.of("Elements", List.of("Text Box", "Check Box", "Radio Button", "Web Tables", "Buttons", "Links", "Broken Links - Images", "Upload and Download", "Dynamic Properties")),
+                Arguments.of("Forms", List.of("Practice Form"))
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest
+    @Tag("navigation_panel")
+    void navigationPanelTest(String groupHeader, List<String> labels) {
+        open("/login");
+        $(".accordion").$(byText(groupHeader)).click();
+        $$(".menu-list li").filter(visible).shouldHave(CollectionCondition.texts(labels));
+    }
+}
